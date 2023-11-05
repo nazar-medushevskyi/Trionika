707ab7cbe6c1283b7dd8a2b45176e71ad6bb1b09
Nazar <nazar378378379379@gmail.com>
2023-11-05 13:52:56 +0200

    FixSectionEleven

--- my-react-app/src/Components/Pages/HomePage/Section-11/SectionEleven.jsx
+++ my-react-app/src/Components/Pages/HomePage/Section-11/SectionEleven.jsx
@@ -6,144 +6,72 @@ import Four from './Images/VectorFour.svg';
 
 const imagesItems = [One, Two, Three, Four];
 
+
 const headerContent = [
-  {
-    id: 1,
-    image: imagesItems[0],
-    title: 'Silver'
-  },
+  { id: 1, image: imagesItems[0], title: 'Silver' },
 
-  {
-    id: 2,
-    image: imagesItems[1],
-    title: 'Gold'
-  },
+  { id: 2, image: imagesItems[1], title: 'Gold' },
 
-  {
-    id: 3,
-    image: imagesItems[2],
-    title: 'No name'
-  },
+  { id: 3, image: imagesItems[2], title: 'No name' },
 
-  {
-    id: 4,
-    image: imagesItems[3],
-    title: 'General'
-  },
-]
+  { id: 4, image: imagesItems[3], title: 'General' },
+];
+
 
 const dataBaseNameOne = [
-  {
-    number: '1',
-    name: 'Name'
-  },
 
-  {
-    number: '3',
-    name: 'Name v2'
-  },
+  { number: '1', name: 'Name' },
 
-  {
-    number: '4',
-    name: 'Name v3'
-  },
+  { number: '3', name: 'Name v2' },
 
-  {
-    number: '7',
-    name: 'Name v4'
-  },
+  { number: '7', name: 'Name v4' },
 
-  {
-    number: '8',
-    name: 'Name v4'
-  },
+  { number: '8', name: 'Name v4' },
 
-  {
-    number: '10',
-    name: 'Name v4'
-  },
+  { number: '10', name: 'Name v4' },
 ];
+
 
 const dataBaseNameTwo = [
-  {
-    number: '11',
-    name: 'Name'
-  },
 
-  {
-    number: '12',
-    name: 'Name v2'
-  },
+  { number: '11', name: 'Name' },
 
-  {
-    number: '14',
-    name: 'Name v3'
-  },
+  { number: '12', name: 'Name v2' },
 
-  {
-    number: '17',
-    name: 'Name v4'
-  },
+  { number: '14', name: 'Name v3' },
 
-  {
-    number: '18',
-    name: 'Name v4'
-  },
+  { number: '17', name: 'Name v4' },
 
-  {
-    number: '20',
-    name: 'Name v4'
-  },
+  { number: '18', name: 'Name v4' },
+
+  { number: '20', name: 'Name v4' },
 ];
+
 
 const dataBaseNameThree = [
-  {
-    number: '21',
-    name: 'Name'
-  },
 
-  {
-    number: '22',
-    name: 'Name v2'
-  },
+  { number: '21', name: 'Name' },
 
-  {
-    number: '24',
-    name: 'Name v3'
-  },
+  { number: '22', name: 'Name v2' },
 
-  {
-    number: '25',
-    name: 'Name v4'
-  },
+  { number: '24', name: 'Name v3' },
 
-  {
-    number: '27',
-    name: 'Name v4'
-  },
+  { number: '25', name: 'Name v4' },
 
-  {
-    number: '33',
-    name: 'Name v4'
-  },
+  { number: '27', name: 'Name v4' },
+
+  { number: '33', name: 'Name v4' },
 ];
 
+
 const dataBaseNameFour = [
-  {
-    number: '34',
-    name: 'Name'
-  },
+  { number: '34', name: 'Name' },
 
-  {
-    number: '35',
-    name: 'Name v2'
-  },
+  { number: '35', name: 'Name v2' },
 
-  {
-    number: '36',
-    name: 'Name v3'
-  },
-]
+  { number: '36', name: 'Name v3' },
+];
+
 
 export const SectionEleven = () => {
   return (
@@ -172,35 +100,35 @@ export const SectionEleven = () => {
 
           <div className="sectionEleven__content-part__mainContent">
 
-              <div className="sectionEleven__content-part__blockOne">
-                {dataBaseNameOne.map((el, index) => (
-                  <div key={index} className="sectionEleven__content-part__blockOne-infoOne">
-                    <p className='sectionEleven__content-part__text'>
-                      {el.number}
-                    </p>
+            <div className="sectionEleven__content-part__blockOne">
+              {dataBaseNameOne.map((el, index) => (
+                <div key={index} className="sectionEleven__content-part__blockOne-infoOne">
+                  <p className='sectionEleven__content-part__text'>
+                    {el.number}
+                  </p>
 
-                    <p className='sectionEleven__content-part__text'>
-                      {el.name}
-                    </p>
-                  </div>
-                ))}
+                  <p className='sectionEleven__content-part__text'>
+                    {el.name}
+                  </p>
                 </div>
+              ))}
+            </div>
 
-                <div className="sectionEleven__content-part__blockTwo">
-                  {dataBaseNameTwo.map((el, index) => (
-                    <div key={index} className="sectionEleven__content-part__blockTwo-infoTwo">
-                      <p className='sectionEleven__content-part__text'>
-                        {el.number}
-                      </p>
+            <div className="sectionEleven__content-part__blockTwo">
+              {dataBaseNameTwo.map((el, index) => (
+                <div key={index} className="sectionEleven__content-part__blockTwo-infoTwo">
+                  <p className='sectionEleven__content-part__text'>
+                    {el.number}
+                  </p>
 
-                      <p className='sectionEleven__content-part__text'>
-                        {el.name}
-                      </p>
-                    </div>
-                  ))}
+                  <p className='sectionEleven__content-part__text'>
+                    {el.name}
+                  </p>
                 </div>
-              
-           
+              ))}
+            </div>
+
+
 
 
             <div className="sectionEleven__content-part__blockThree">
@@ -215,28 +143,28 @@ export const SectionEleven = () => {
                   </p>
                 </div>
               ))}
-              </div>
+            </div>
 
-              <div className="sectionEleven__content-part__blockFour">
-                {dataBaseNameFour.map((el, index) => (
-                  <div key={index} className="sectionEleven__content-part__blockFour-infoTwo">
-                    <p className='sectionEleven__content-part__text'>
-                      {el.number}
-                    </p>
+            <div className="sectionEleven__content-part__blockFour">
+              {dataBaseNameFour.map((el, index) => (
+                <div key={index} className="sectionEleven__content-part__blockFour-infoTwo">
+                  <p className='sectionEleven__content-part__text'>
+                    {el.number}
+                  </p>
 
-                    <p className='sectionEleven__content-part__text'>
-                      {el.name}
-                    </p>
-                  </div>
-                ))}
-              </div>
-            
+                  <p className='sectionEleven__content-part__text'>
+                    {el.name}
+                  </p>
+                </div>
+              ))}
+            </div>
+
 
 
           </div>
 
 
-        
+
 
 
         </div>
